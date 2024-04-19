16fc0b919125e0c03a13d7c47e5632d779eafd9b
natasabrunet <natasabrunet731@gmail.com>
2024-04-19 21:06:53 +0530

    Image added
    That'sThe, GoodLife images added

--- src/pages/Step0/Step0.js
+++ src/pages/Step0/Step0.js
@@ -12,6 +12,9 @@ import './Step0.scss'
 import ThatsIcon from 'assets/images/thatsIcon.svg'
 import BrokenGoodLife from 'assets/images/brokenGoodLife.svg'
 import { setLang } from 'redux/slices/lang'
+import ThatsTheLogo from 'assets/images/leafs/thatsthe.png';
+import GoodLifeLogo from 'assets/images/leafs/goodlife.png';
+import SignatureLogo from 'assets/images/leafs/signature.png';
 
 const Step0 = () => {
 	const [langIsEnglish, setLangIsEnglish] = useState(true)
@@ -45,14 +48,23 @@ const Step0 = () => {
 	return (
 		<Layout isHome={true} info={false} setting={false}>
 			<div className='Step0'>
+				<div className="thats-the-logo">
+				<img src={ThatsTheLogo} alt="That's The" />
+				</div>
 				<div className='content'>
 					<h1>
 					YOU COULD WIN $10,000 IN PERSONAL TRAINING
 					</h1>
+					<div className="signature-logo">
+				<img src={SignatureLogo} alt="Signature" />
+				</div>
 					<h4>
 					Prize includes TWO 1-year GoodLife Fitness gym memberships.
 					</h4>
 					<Link to={'/step1'}>ENTER NOW</Link>
+				</div>
+				<div className="goodlife-logo">
+				<img src={GoodLifeLogo} alt="Good Life" />
 				</div>
 				<p>
 				No purchase necessary to enter the Contest. Contest begins on April 26, 2024 and ends of October 19, 2024. Contest open to those who attend any of the Lead Gen activations being conducted throughout Canada, other than those who reside in the province of Quebec and who are not employees of GoodLife Fitness Centres Inc. or Influence Retail Services Inc. To participate, complete the on-screen instructions via iPad or on your own mobile device via the on-site QR code at the GoodLife booth. The names of all valid participants will be entered into a Grand Prize draw for the chance to win a 1-year GoodLife Fitness ‘Performance’ Membership and a $10,000 personal training package. Approximate retail value of $11,500 CAD. Grand Prize draw will take place on Monday, October 28, 2024 and the Winner will be contacted on that date. Other conditions apply. Visit www.GLMembership.ca for more details and for full Rules and Regulations. This contest is administered by Influence Retail Services Inc. who retains sole responsibility of the Contest.
